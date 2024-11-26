6a9c4f378cb20a351492df7072655fa99e43ccbd
Efrat Gefen <efratg736@gmail.com>
2024-11-26 15:22:58 +0200

    The Django server is running successfully and handling requests, but there is a problem accessing static files.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
-# Set default parameters
+# Wait for PostgreSQL to start
 DATABASE_HOST=${DATABASE_HOST:-"postgres"}
 DATABASE_PORT=${DATABASE_PORT:-"5432"}
 
 echo "Waiting for PostgreSQL to start..."
-
-# Loop to wait until PostgreSQL is up
-until nc -z -v -w30 $DATABASE_HOST $DATABASE_PORT
-do
+until nc -z -v -w30 $DATABASE_HOST $DATABASE_PORT; do
   echo "Unable to connect to $DATABASE_HOST:$DATABASE_PORT. Waiting 5 seconds..."
   sleep 5
 done
 
 echo "PostgreSQL is up, applying migrations..."
+python manage.py migrate  # הרצת המיגרציות
 
-# Apply migrations
-python manage.py migrate
+echo "Collecting static files..."
+python manage.py collectstatic --noinput  # אוסף קבצים סטטיים
 
 echo "Starting Django..."
-
-# Run Django
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000  # מפעיל את Django
--- gutendex/settings.py
+++ gutendex/settings.py
@@ -39,7 +39,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+# ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['*'] 
 
 
 # Application definition
@@ -142,14 +143,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = env('STATIC_ROOT')
+# STATIC_ROOT = env('STATIC_ROOT')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
+# # User-uploaded files
+# MEDIA_ROOT = env('MEDIA_ROOT')
+# MEDIA_URL = '/media/'
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
+STATIC_ROOT = '/app/staticfiles'  # תיקיית הסטטיים ב-Docker
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-
-# User-uploaded files
-MEDIA_ROOT = env('MEDIA_ROOT')
+# User-uploaded files (Media)
 MEDIA_URL = '/media/'
+MEDIA_ROOT = '/app/media'  # תיקיית המדיה ב-Docker
+
 
 
 # Users for error reporting
